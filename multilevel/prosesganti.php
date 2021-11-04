4245e8c9182d9fd6fe8357999490ee9af78270ed
rizkyrendhi <rendhi782@gmail.com>
2021-11-04 11:37:06 +0700

    Update prosesganti.php

--- multilevel/prosesganti.php
+++ multilevel/prosesganti.php
@@ -1,27 +1,26 @@
 <?php
 
-//panggil koneksi database
+
 include "koneksi.php";
 
-//enkripsi inputan password lama
+
 $password_lama = $_POST['pass_lama'];
 
-//panggil username
+
 $username = $_POST['username'];
 
-//uji jika password lama sesuai
+
 $tampil = mysqli_query($koneksi, "SELECT * FROM loginmulti WHERE 
                                 username = '$username' and password = '$password_lama'");
 $data = mysqli_fetch_array($tampil);
-//jika data ditemukan, maka password sesuai
+
 if ($data) {
-    //uji jika password baru dan konfirmasi password sama
+    
     $password_baru = $_POST['pass_baru'];
     $konfirmasi_password = $_POST['konfirmasi_pass'];
 
     if ($password_baru == $konfirmasi_password) {
-        //proses ganti password
-        //enkripsi password baru
+       
         $pass_ok = $konfirmasi_password;
         $ubah = mysqli_query($koneksi, "UPDATE loginmulti set password = '$pass_ok'  
                                         WHERE id = '$data[id]' ");

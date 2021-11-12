b2beb071b71fd565a0a190c38c4f0830f28fbcba
rizkyrendhi <rendhi782@gmail.com>
2021-11-12 10:47:42 +0700

    Update hapus1.php

--- hapus1.php
+++ hapus1.php
@@ -9,13 +9,13 @@ if ( hapus_1($id) > 0) {
 	echo 
 			"<script>
 				alert('data berhasil dihapus');
-				document.location.href = 'admin-sekolah-5.php';
+				document.location.href = 'admin_3.php';
 			</script>";
 }else{
 	echo 
 			"<script>
 				alert('data gagal dihapus');
-				document.location.href = 'admin-sekolah-5.php';
+				document.location.href = 'admin_3.php';
 			</script>";
 }
 
